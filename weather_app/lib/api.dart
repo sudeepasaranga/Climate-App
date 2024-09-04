f5db52ceff65e99abf21d1dd6d520b7223bdcd8d
sudeepasaranga <sarangasadeepa@gmail.com>
2024-09-04 16:52:24 +0530

    Create weathermodel.dart & change the homepage.dart

--- weather_app/lib/api.dart
+++ weather_app/lib/api.dart
@@ -1,15 +1,18 @@
+import 'dart:convert';
+
 import 'package:weather_app/constants.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/weathermodel.dart';
 
 class WeatherApi{
-  final String baseUrl = "http://api.weatherapi.com/v1///current.json";
+  final String baseUrl = "http://api.weatherapi.com/v1/current.json";
 
-    getCurrentWeather(String location) async{
+    Future<ApiResponse> getCurrentWeather(String location) async{
         String apiUrl = "$baseUrl?Key=$apiKey&q=$location";
         try{
           final responce = await http.get(Uri.parse(apiUrl));
           if(responce.statusCode==200){
-            print(responce.body);
+            return ApiResponse.fromJson(jsonDecode(responce.body));
 
           }else{
             throw Exception("Failed to load weather");
--- weather_app/lib/homepage.dart
+++ weather_app/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/api.dart';
+import 'package:weather_app/weathermodel.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,6 +10,10 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+
+  ApiResponse? response;
+  bool inProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold(
@@ -16,7 +21,10 @@ class _HomepageState extends State<Homepage> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            _buildSearchWidget()
+            _buildSearchWidget(),
+            const SizedBox(height: 20),
+            if(inProgress)
+            CircularProgressIndicator() else _buildSearchWidget(),
           ],
         ),
       ),
@@ -32,7 +40,99 @@ class _HomepageState extends State<Homepage> {
     );
    }
 
+   Widget _buildSearchWidget() {
+      if (response == null){
+        return Text("Search for the location to get weather data");
+      }else{
+        return Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Icon(
+                  Icons.location_on,
+                  size: 50,
+                  ),
+                  Text(
+                    response?.location?.name ?? "",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w300,
+
+                    ),
+                  ),
+                  Text(
+                    response?.location?.country ?? "",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:Text(
+                        (response?.current?.tempC.toString() ?? "")+" °c",
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  ),
+                        
+                  Text(
+                    (response?.current?.condition?.text.toString() ?? "")+" °c",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              ),
+              Center(
+                child: SizedBox(
+                  height: 200,
+                  child:Image.network(
+                    "https:${response?.current?.condition?.icon}"
+                    .replaceAll("64x64","128x128" ),
+                    scale: 0.7,
+                  )
+                )
+              ),
+
+              Card(
+                elevation: 4,
+                color: Colors.white,
+              )
+      
+             ],
+        );
+      }
+   }
+
    _getWeatherData(String location)async{
-      await WeatherApi().getCurrentWeather(location);
+    setState(() {
+      inProgress = true;
+    });
+
+    try{
+
+       response = await WeatherApi().getCurrentWeather(location);
+    }catch(e){
+
+    }finally{
+
+      setState(() {
+        inProgress = false;
+      });
+    }
+      
+      
    }
 }
