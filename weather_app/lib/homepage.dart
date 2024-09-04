ffe2d6fe33630a95e7b43db8cc59e4f216d497e3
sudeepasaranga <sarangasadeepa@gmail.com>
2024-09-04 19:07:55 +0530

    last commit

--- weather_app/lib/homepage.dart
+++ weather_app/lib/homepage.dart
@@ -10,214 +10,211 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
   ApiResponse? response;
   bool inProgress = false;
   String message = "Search for the location to get weather data";
 
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            _buildSearchWidget(),
-            const SizedBox(height: 20),
-            if(inProgress)
-            CircularProgressIndicator() 
-            else 
-            Expanded(
-              child:  SingleChildScrollView(child:  _buildWeatherWidget())
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xFF2193b0), Color(0xFF6dd5ed)],
             ),
-           
-          ],
+          ),
+          child: Column(
+            children: [
+              _buildSearchWidget(),
+              const SizedBox(height: 20),
+              if (inProgress)
+                const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                )
+              else
+                Expanded(
+                  child: SingleChildScrollView(child: _buildWeatherWidget()),
+                ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 
-   Widget _buildSearchWidget(){
+  Widget _buildSearchWidget() {
     return SearchBar(
       hintText: "Search any location",
-      onSubmitted: (value){
+      onSubmitted: (value) {
         _getWeatherData(value);
       },
     );
-   }
+  }
 
-   Widget _buildWeatherWidget() {
-      if (response == null){
-        return Text(message);
-      }else{
-        return Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Icon(
-                  Icons.location_on,
-                  size: 50,
-                  ),
-                  Text(
-                    response?.location?.name ?? "",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w300,
-
-                    ),
-                  ),
-                  Text(
-                    response?.location?.country ?? "",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child:Text(
-                        (response?.current?.tempC.toString() ?? "")+" °c",
-                        style: TextStyle(
-                          fontSize: 60,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                  ),
-                        
-                  Text(
-                    (response?.current?.condition?.text.toString() ?? "")+" °c",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  )
-                ],
-              ),
-              Center(
-                child: SizedBox(
-                  height: 200,
-                  child:Image.network(
-                    "https:${response?.current?.condition?.icon}"
-                    .replaceAll("64x64","128x128" ),
-                    scale: 0.7,
-                  )
-                )
-              ),
-
-              Card(
-                elevation: 4,
+  Widget _buildWeatherWidget() {
+    if (response == null) {
+      return Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Icon(
+                Icons.location_on,
+                size: 50,
                 color: Colors.white,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        _dataAndTitleWidget("Humidity",
-                            response?.current?.humidity?.toString() ?? ""),
-                        _dataAndTitleWidget("Wind Speed",
-                         "${response?.current?.windKph?.toString() ?? ""} + km/h")
-                      ],
-                    ),
-
-                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        _dataAndTitleWidget("UV",
-                            response?.current?.uv?.toString() ?? ""),
-                        _dataAndTitleWidget("Percipitation",
-                         "${response?.current?.precipMm?.toString() ?? ""} + mm")
-                      ],
-                    ),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        _dataAndTitleWidget("Local Time",
-                            response?.location?.localtime?.split(" ").last ?? ""),
-                        _dataAndTitleWidget("Local Date",
-                            response?.location?.localtime?.split(" ").first ?? ""),
-                      ],
-                    ),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        _dataAndTitleWidget("Humidity",
-                            response?.current?.humidity?.toString() ?? ""),
-                        _dataAndTitleWidget("Wind Speed",
-                         "${response?.current?.windKph?.toString() ?? ""} + km/h")
-                      ],
-                    ),
-
-                  ],
+              ),
+              Text(
+                response?.location?.name ?? "",
+                style: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                  fontFamily: 'Poppins'
+                ),
+              ),
+              Text(
+                response?.location?.country ?? "",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white70,
+                  fontFamily: 'Poppins'
                 ),
               )
-      
-             ],
-        );
-      }
-   }
-
-   Widget _dataAndTitleWidget(String title,String data){
-
-        return Padding(
-          padding: const EdgeInsets.all(18.0),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  (response?.current?.tempC.toString() ?? "") + " °C",
+                  style: const TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Poppins'
+                  ),
+                ),
+              ),
+              Text(
+                response?.current?.condition?.text.toString() ?? "",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white70,
+                  fontFamily: 'Poppins'
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: SizedBox(
+              height: 200,
+              child: Image.network(
+                "https:${response?.current?.condition?.icon}"
+                    .replaceAll("64x64", "128x128"),
+                scale: 0.7,
+              ),
+            ),
+          ),
+          Card(
+            elevation: 4,
+            color: Colors.white70,
             child: Column(
               children: [
-                Text(
-                  data,
-                  style: const TextStyle(
-                    color:Colors.black87,
-                    fontWeight: FontWeight.w600,
-
-                  ), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _dataAndTitleWidget("Humidity",
+                        response?.current?.humidity?.toString() ?? ""),
+                    _dataAndTitleWidget(
+                        "Wind Speed",
+                        "${response?.current?.windKph?.toString() ?? ""} km/h"),
+                  ],
                 ),
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 27,
-                    color:Colors.black87,
-                    fontWeight: FontWeight.w600,
-
-                  ), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _dataAndTitleWidget(
+                        "UV", response?.current?.uv?.toString() ?? ""),
+                    _dataAndTitleWidget(
+                        "Precipitation",
+                        "${response?.current?.precipMm?.toString() ?? ""} mm"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _dataAndTitleWidget("Local Time",
+                        response?.location?.localtime?.split(" ").last ?? ""),
+                    _dataAndTitleWidget("Local Date",
+                        response?.location?.localtime?.split(" ").first ?? ""),
+                  ],
                 ),
               ],
-          ) ,
-        );
+            ),
+          )
+        ],
+      );
+    }
+  }
 
-   }
+  Widget _dataAndTitleWidget(String title, String data) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(
+        children: [
+          Text(
+            data,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Poppins'
+            ),
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 27,
+              color: Colors.black87,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Poppins'
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-   _getWeatherData(String location)async{
+  _getWeatherData(String location) async {
     setState(() {
       inProgress = true;
     });
 
-    try{
-
-       response = await WeatherApi().getCurrentWeather(location);
-    }catch(e){
+    try {
+      response = await WeatherApi().getCurrentWeather(location);
+    } catch (e) {
       setState(() {
         message = "Failed to get weather";
         response = null;
-
       });
-
-    }finally{
-
+    } finally {
       setState(() {
         inProgress = false;
       });
     }
-      
-      
-   }
+  }
 }
